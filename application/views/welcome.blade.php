c696ebb31da2e1181a98186381d7333cf77d5e94
Tim Joosten <Tjoosten3@gmail.com>
2017-01-11 21:09:44 +0100

    Fix #2 | Needed the text for this game.

--- application/views/welcome.blade.php
+++ application/views/welcome.blade.php
@@ -8,8 +8,28 @@
     </div>
 
     <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-9 padding-top">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h2>Belgisch Kampioenschap van de wansmaak</h2>
 
+                    <p>
+                        Onze politici geven zichzelf erg graag complimentjes en goede rapporten. Alles is een success,elke uitspraak is gerechtvaardigd, en elke opinie is redelijk, bedaard en gematigd.
+                    </p>
+
+                    <p>
+                        Maar net zoals in school is het rapport dat dat de leerling zichzelf geeft niet echt betrouwbaar.
+                        Zo ook met onze politici. De burgers zijn degene die hen moeten beoordelen, niemand anders.
+                        Daarom deze site.
+                    </p>
+
+                    <p>
+                        Geef onze politici hier punten voor wansmaak. Telkens ze iets verklaren of beargumenteren kan
+                        je hen een score geven. En op het einde van het jaar reiken we een trofee uit aan degene
+                        die het hoogst scoort in de wansmaak-index. Die wordt dan Belgisch Kampioen van de Wansmaak.
+                    </p>
+                </div>
+            </div>
         </div>
 
         <div class="col-sm-3 padding-top">
