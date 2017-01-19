94d250e9715e9fe72996f216b8a4486713222d6b
Tim Joosten <Tjoosten3@gmail.com>
2017-01-19 19:10:51 +0100

    Init empty controllers

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -27,4 +27,50 @@ class Users extends MY_Controller
     {
         parent::__construct();
     }
+
+    /**
+     * Middleware instance
+     *
+     * only create if you want to use, not compulsory.
+     * or return parent::middleware(); if you want to keep.
+     * or return empty array() and no middleware will run.
+     *
+     * @return array
+     */
+    protected function middleware()
+    {
+        // Return the list of middlewares you want to be applied,
+        // Here is list of some valid options
+        //
+        // admin_auth                    // As used below, simplest, will be applied to all
+        // someother|except:index,list   // This will be only applied to posts()
+        // yet_another_one|only:index    // This will be only applied to index()
+        //
+        return ['admin_auth'];
+    }
+
+    public function updatePermissions()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function search()
+    {
+
+    }
+
+    public function status()
+    {
+
+    }
+
+    public function paginationConfig()
+    {
+
+    }
 }
