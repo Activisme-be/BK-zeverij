3ab3c5a98033e5c9c394ecbedaedbf5581b8f254
Tim Joosten <Tjoosten3@gmail.com>
2017-01-25 02:24:36 +0100

    Implement validation errors

--- application/views/problem/index.blade.php
+++ application/views/problem/index.blade.php
@@ -18,23 +18,31 @@
 						 {{-- CSRF --}}
     					<input type="hidden" name="{{ $this->security->get_csrf_token_name() }}" value="{{ $this->security->get_csrf_hash() }}">
 
-    					<div class="form-group">
+    					<div class="form-group {{ form_error('title') ? 'has-error' : '' }}">
     						<label class="control-label col-sm-3">
     							Titel probleem: <span class="text-danger">*</span>
     						</label>
 
     						<div class="col-sm-7">
     							<input type="text" name="title" class="form-control" placeholder="Titel probleem">
+
+    							 @if (form_error('title'))
+                                    <span class="help-block"><small>{{ form_error('title') }}</small></span>
+                                @endif
     						</div>
     					</div>
 
-						<div class="form-group">
+						<div class="form-group {{ form_error('description') ? 'has-error' : '' }}">
 							<label class="control-label col-sm-3">
 								Beschrijving: <span class="text-danger">*</span>
 							</label>
 
 							<div class="col-sm-9">
 								<textarea rows="8" name="description" placeholder="Beschrijving van het probleem" class="form-control"></textarea>
+
+								@if (form_error('description'))
+                                    <span class="help-block"><small>{{ form_error('description') }}</small></span>
+                                @endif
 							</div>
 						</div>
 
