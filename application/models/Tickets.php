54da21ac68efa4f26c89ff378f331414fdfa9121
Tim Joosten <Tjoosten3@gmail.com>
2017-01-25 01:53:15 +0100

    Implement ticket model

---
+++ application/models/Tickets.php
@@ -0,0 +1,44 @@
+<?php defined('BASEPATH') or exit('no direct script access allowed');
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Tickets Model.
+ *
+ * @author    Tim Joosten   <Topairy@gmail.com>
+ * @copyright Activisme-BE  <info@activisme.be>
+ * @license:  MIT license
+ * @since     2017
+ * @package   BK-wansmaak
+ */
+class Tickets extends Model 
+{
+	/**
+     * Set the database connection. 
+     * 
+     * @return string
+     */
+    protected $connection = 'resources';
+
+
+	/** 
+	 * Enable / Disable timestamps. 
+	 * 
+	 * @return bool
+	 */
+	public $timestamps = true; 
+
+	/** 
+	 * Set the database table. 
+	 * 
+	 * @return string
+	 */ 
+	protected $table = 'tickets'; 
+
+	/** 
+	 * Mass-assign fields. 
+	 * 
+	 * @return array
+	 */ 
+	protected $fillable = ['title', 'description'];
+}
