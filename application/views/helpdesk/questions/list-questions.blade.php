d96817c17a57bc7e5cccdc46801c7531472c5718
Tjoosten <Tjoosten3@gmail.com>
2017-02-02 08:55:11 +0100

    Work on user specific issues

--- application/views/helpdesk/questions/list-questions.blade.php
+++ application/views/helpdesk/questions/list-questions.blade.php
@@ -2,12 +2,46 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm-offset-1 col-sm-10">
+        <div class="col-sm-12">
             <div class="panel panel-default">
                 <div class="panel-heading"><span class="fa fa-info-circle" aria-hidden="true"></span> Vragen:</div>
 
                 <div class="panel-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Categorie:</th>
+                                <th>Vraag:</th>
+                                <th colspan="2">Geopend op:</th> {{-- Colspan 2 needed because the functions are embedded. --}}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($questions as $question)
+                                <tr>
+                                    <td><code>#{{ $question->id }}</code></td>
 
+                                    <td>
+                                        @foreach($question->category as $tag)
+                                            <span class="label label-primary"> {{ $tag->category }} </span>
+                                        @endforeach
+                                    </td>
+
+                                    <td> {{ $question->title }} </td>
+                                    <td> {{ $question->created_at }} </td>
+
+                                    {{-- Functions --}}
+                                        <td>
+                                            <a href="#" class="label label-success">Bekijken</a>
+                                            <a href="{{ base_url('questions/status/' . . '/' .) }}" class="label label-danger">
+                                                Sluiten
+                                            </a>
+                                        </td>
+                                    {{-- /Functions --}}
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
