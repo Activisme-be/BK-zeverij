024ba291143a7a4c02ae790be8180944ef22c89d
Tjoosten <Tjoosten3@gmail.com>
2017-02-03 08:55:53 +0100

    Add static view for notifications

--- application/views/layouts/front-end.blade.php
+++ application/views/layouts/front-end.blade.php
@@ -50,6 +50,46 @@
 
                     <ul class="nav navbar-nav navbar-right">
                         @if ($this->user)
+                            {{-- Notifications --}}
+                                <li class="dropdown dropdown-notifications">
+                                    <a href="#notifications-panel" class="dropdown-toggle" data-toggle="dropdown">
+                                        <span data-count="2" class="glyphicon glyphicon-bell notification-icon"></span>
+                                    </a>
+
+                                    <div class="dropdown-container dropdown-menu-right">
+
+                                        <div class="dropdown-toolbar">
+                                            <h3 class="dropdown-toolbar-title">Notificaties (2)</h3>
+                                        </div> {{-- /dropdown toolbar  --}}
+
+                                        <ul class="dropdown-menu notifications">
+                                            <li class="notification">
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <div class="media-object">
+                                                            <img data-src="holder.js/50x50?bg=cccccc" class="img-circle" alt="50x50" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2250%22%20height%3D%2250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2050%2050%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15a02e2608f%20text%20%7B%20fill%3A%23919191%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15a02e2608f%22%3E%3Crect%20width%3D%2250%22%20height%3D%2250%22%20fill%3D%22%23cccccc%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%226.2265625%22%20y%3D%2229.5328125%22%3E50x50%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 50px; height: 50px;">
+                                                        </div>
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <strong class="notification-title"><a href="#">Dave Lister</a> commented on <a href="#">DWARF-13 - Maintenance</a></strong>
+                                                        <p class="notification-desc">I totally don't wanna do it. Rimmer can do it.</p>
+
+                                                        <div class="notification-meta">
+                                                            <small class="timestamp">27. 11. 2015, 15:00</small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+
+                                        <div class="dropdown-footer text-center">
+                                            <a href="#">Bekijk alles</a>
+                                        </div>{{-- /dropdown-footer --}}
+
+                                    </div>
+                                </li>
+                            {{-- /Notifications --}}
+
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                                     {{ $this->user['username'] }}
