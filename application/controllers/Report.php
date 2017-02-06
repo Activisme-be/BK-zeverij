8646a994094dad42ade99e2062fb663156ece8e9
Tim Joosten <Tjoosten3@gmail.com>
2017-02-06 18:27:26 +0100

    Update

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -1,27 +1,62 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * TODO: Implement copyright block.
+ * Report Controller.
+ *
+ * @author    Tim Joosten   <Topairy@gmail.com>
+ * @copyright Activisme-BE  <info@activisme.be>
+ * @license:  MIT license
+ * @since     2017
+ * @package   BK-wansmaak
  */
 class Report extends MY_Controller
 {
+    /**
+     * Report Constructor.
+     *
+     * @return void
+     */
     public function __construct()
     {
-
+        parent::__construct();
     }
 
+    /**
+     * 
+     *
+     * @see
+     * @return
+     */
     public function view()
     {
-
+        $reportId = $this->security->xss_clean($this->uri->segment(3));
     }
 
+    /**
+     *
+     *
+     * @see
+     * @return
+     */
     public function delete()
     {
+        $reportId = $this->seucrity->xss_clean($this->uri->segment(3));
 
+        if (Reports::find($reportId)->destroy()) {
+            $this->session->set_flashdata('class', 'alert alert-success');
+            $this->session->set_flashdata('message', '');
+        }
     }
 
     public function close()
     {
         $reportId = $this->security->xss_clean($this->uri->segment(3));
+
+        if () {
+            $this->session->set_flashdata('class', '');
+            $this->session->set_flashdata('message', '');
+        }
+
+        return redirect(base_url('/news/backend'));
     }
 }
